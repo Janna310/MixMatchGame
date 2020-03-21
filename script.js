3b7012e49c824fe5e76aae75ef21d9371269e19e
Janna310 <57925340+Janna310@users.noreply.github.com>
2020-03-21 11:55:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,124 @@
-// possibly adding in audio for background music, "victory", and "loser"
-
-
-//running the game function
-class MixOrMatch{
-    constructor(totalTime, cardsArray){ //cards Array Parameter not reading
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remaining');
-        this.ticker = document.getElementById('flips')
+class MixOrMatch {
+    constructor(totalTime, cards) {
+      this.cardsArray = cards;
+      this.totalTime = totalTime;
+      this.timeRemaining = totalTime;
+      this.timer = document.getElementById("time-remaining");
+      this.ticker = document.getElementById("flips");
     }
-    startGame(){
-        this.cardToCheck = null;
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.matchedCards = [];
-        this.busy = true;
+  
+    startGame() {
+      this.totalClicks = 0;
+      this.timeRemaining = this.totalTime;
+      this.cardToCheck = null;
+      this.matchedCards = [];
+      this.busy = true;
+      setTimeout(() => { //do we have a timeout function? 
+        this.shuffleCards(this.cardsArray);
+        this.countdown = this.startCountdown();
+        this.busy = false;
+      }, 500);
+      this.hideCards(); //where does this come in?
+      this.timer.innerText = this.timeRemaining;
+      this.ticker.innerText = this.totalClicks;
     }
-    flipCard(card){ //function to flip cards over and apply pre-created visible style
-        if(this.canFlipCard(card)) {
-            this.totalClicks++;
-            this.ticker.innerText = this.totalClicks;
-            card.classList.add('visible')
+    startCountdown() {
+      return setInterval(() => {
+        this.timeRemaining--;
+        this.timer.innerText = this.timeRemaining;
+        if (this.timeRemaining === 0) this.gameOver();
+      }, 1000);
+    }
+    gameOver() {
+      clearInterval(this.countdown);
+      document.getElementById("game-over-text").classList.add("visible");
+    }
+    victory() {
+      clearInterval(this.countdown);
+      document.getElementById("victory-text").classList.add("visible");
+    }
+    hideCards() {
+      this.cardsArray.forEach(cards => {
+        cards.classList.remove("visible");
+        cards.classList.remove("matched");
+      });
+    }
+    flipCard(cards) {
+      if (this.canFlipCard(cards)) {
+        this.totalClicks++;
+        this.ticker.innerText = this.totalClicks;
+        cards.classList.add("visible");
+  
+        if (this.cardToCheck) {
+          this.checkForCardMatch(cards);
+        } else {
+          this.cardToCheck = cards;
         }
-
+      }
     }
-    canFlipCard(card) {
-        return true
-        // return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck
+    checkForCardMatch(cards) {
+      if (this.getCardType(cards) === this.getCardType(this.cardToCheck))
+        this.cardMatch(cards, this.cardToCheck);
+      else this.cardMismatch(cards, this.cardToCheck);
+  
+      this.cardToCheck = null;
     }
-}
-
-
-
-//removing overlay screens (class =overlay-text)
-function ready(){
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let card = Array.from(document.getElementsByClassName('cards'));
+    cardMatch(card1, card2) {
+      this.matchedCards.push(card1);
+      this.matchedCards.push(card2);
+      card1.classList.add("matched");
+      card2.classList.add("matched");
+      if (this.matchedCards.length === this.cardsArray.length) this.victory();
+    }
+    cardMismatch(card1, card2) {
+      this.busy = true;
+      setTimeout(() => {
+        card1.classList.remove("visible");
+        card2.classList.remove("visible");
+        this.busy = false;
+      }, 1000);
+    }
+    shuffleCards(cardsArray) {
+      // Fisher-Yates Shuffle Algorithm.
+      for (let i = cardsArray.length - 1; i > 0; i--) {
+        let randIndex = Math.floor(Math.random() * (i + 1));
+        cardsArray[randIndex].style.order = i;
+        cardsArray[i].style.order = randIndex;
+      }
+    }
+    getCardType(cards) {
+      return cards.getElementsByClassName("card-value")[0].src;
+    }
+    canFlipCard(cards) {
+      return (
+        !this.busy &&
+        !this.matchedCards.includes(cards) &&
+        cards !== this.cardToCheck
+      );
+    }
+  }
+  
+  if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+  } else {
+    ready();
+  }
+  
+  function ready() {
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let cards = Array.from(document.getElementsByClassName("cards"));
     let game = new MixOrMatch(100, cards);
-
+  
     overlays.forEach(overlay => {
-        overlays.addEventListener('click', () => {
-            overlay.classList.remove('visible')
-            game.startGame(card);
-        });
+      overlay.addEventListener("click", () => {
+        overlay.classList.remove("visible");
+        game.startGame();
+      });
     });
-
-    cards.forEach( card => {
-        cards.addEventListener('click', () => {
-            //game.flipCard(card);
-        });
+  
+    cards.forEach(card => {
+      card.addEventListener("click", () => {
+        game.flipCard(card);
+      });
     });
-}
+  }
